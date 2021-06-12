418d8b4f2c13335e14454097bd63def35d79e5a3
gitstaro <hachimitsu.lab@gmail.com>
2021-06-12 22:26:54 +0900

    change prefrence

--- trac_ik/trac_ik_examples/src/send_frame.cpp
+++ trac_ik/trac_ik_examples/src/send_frame.cpp
@@ -27,6 +27,8 @@ class Frame_pub{
     void frame_pub();
     void callback(const sensor_msgs::Joy::ConstPtr& data);
     geometry_msgs::PoseStamped pos;
+    geometry_msgs::Point rpy;
+    tf::Quaternion rpy_to_tf_quat(double roll, double pitch, double yaw);
   private:
     ros::NodeHandle nh;
     ros::Subscriber sub;
@@ -42,36 +44,45 @@ Frame_pub::Frame_pub()
     pos.pose.position.x=0.564;
     pos.pose.position.y=0.028;
     pos.pose.position.z=0.611;
+    pos.pose.orientation.x= 0;
+    pos.pose.orientation.y= 0;
+    pos.pose.orientation.z= 0;
+    pos.pose.orientation.w= 1.0;
+    // rpy.x=0;
+    // rpy.y=0;
+    // rpy.z=0;
     
 }
-
+tf::Quaternion Frame_pub::rpy_to_tf_quat(double roll, double pitch, double yaw){
+  rpy.x=rpy.x+roll;
+  rpy.y=rpy.y+pitch;
+  rpy.z=rpy.z+yaw;
+  ROS_INFO_STREAM("rpy-"<<rpy.x<<rpy.y<<rpy.z);
+    return tf::createQuaternionFromRPY(rpy.x, rpy.y, rpy.z);
+}
 void Frame_pub::frame_pub() {
     
     pos.header.frame_id="base_link";
     pos.pose.position.x=pos.pose.position.x+joy_pose.position.x;
     pos.pose.position.y=pos.pose.position.y+joy_pose.position.y;
     pos.pose.position.z = pos.pose.position.z+joy_pose.position.z;
-    pos.pose.orientation.x= 0;
-    pos.pose.orientation.y= 0.0001904047369388022;
-    pos.pose.orientation.z= 0;
-    pos.pose.orientation.w= 1.0;
+    pos.pose.orientation.x= joy_pose.orientation.x;
+    pos.pose.orientation.y= joy_pose.orientation.y;
+    pos.pose.orientation.z= joy_pose.orientation.z;
+    pos.pose.orientation.w= joy_pose.orientation.w;
 
     pub.publish(pos);
   }
 
 void Frame_pub::callback(const sensor_msgs::Joy::ConstPtr& data){
-    // if (data->axes[7]<0){
-    // joy_pose.position.z=joy_pose.position.z-0.001;
-    // }
-    // else if(data->axes[7]>0){
-    //      joy_pose.position.z=joy_pose.position.z+0.001;
-    // }
-    // else{
-    //      joy_pose.position.z=joy_pose.position.z;
-    // }
-    joy_pose.position.z= data->axes[1]*0.0002;
-    joy_pose.position.x= (-1.0*data->axes[0])*0.0002;
-    joy_pose.position.y= (-1.0*data->axes[3])*0.0002;
+    joy_pose.position.z= int(data->axes[1])*0.0003;
+    joy_pose.position.x= (-1.0*data->axes[0])*0.0003;
+    joy_pose.position.y= (1.0*data->axes[7])*0.0003;
+    tf::Quaternion joy_quat=rpy_to_tf_quat(int(data->axes[3])*0.05,int(data->axes[4])*0.05,int(data->axes[6])*0.05);
+    joy_pose.orientation.x=joy_quat.getX();
+    joy_pose.orientation.y=joy_quat.getY();
+    joy_pose.orientation.z=joy_quat.getZ();
+    joy_pose.orientation.w=joy_quat.getW();
 }
 
 int main( int argc, char** argv )
